4a9670f518f962870bcb984e9f60e74dfa0aadc7
Z0KRI <david1166@live.com.mx>
2024-12-02 23:59:18 -0600

    🔧 Add env's

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const API_VERSION = configService.get<string>('API_VERSION');
+
+  app.setGlobalPrefix(`api/${API_VERSION}`);
+  await app.listen(configService.get<number>('PORT') ?? 3000);
 }
 bootstrap();
