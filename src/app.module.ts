73764a917321370b8d626f3103b020b4b743a5c6
Z0KRI <david1166@live.com.mx>
2024-12-02 20:42:48 -0600

    🔧 Add microservices

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'INFORMATION_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'information',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'information-consumer',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
