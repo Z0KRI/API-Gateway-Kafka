589305f2a4c95e3df76d2b5468414564a32104e7
Z0KRI <david1166@live.com.mx>
2024-12-05 09:11:48 -0600

    Cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,14 @@ async function bootstrap() {
   const API_VERSION = configService.get<string>('API_VERSION');
 
   app.setGlobalPrefix(`api/${API_VERSION}`);
+
+  // Habilitar CORS para todas las rutas
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   await app.listen(configService.get<number>('PORT') ?? 3000);
 }
 bootstrap();
